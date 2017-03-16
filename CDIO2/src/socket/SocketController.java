1fef92a548d8bd427eedfe9edb722f4730795403
Stuart <Stuart@192.168.0.10>
2017-03-16 19:09:51 +0100

    Threading forsøg

--- CDIO2/src/socket/SocketController.java
+++ CDIO2/src/socket/SocketController.java
@@ -22,12 +22,14 @@ public class SocketController implements ISocketController
 
 
 	@Override
-	public void registerObserver(ISocketObserver observer) {
+	public void registerObserver(ISocketObserver observer) 
+	{
 		observers.add(observer);
 	}
 
 	@Override
-	public void unRegisterObserver(ISocketObserver observer) {
+	public void unRegisterObserver(ISocketObserver observer) 
+	{
 		observers.remove(observer);
 	}
 
@@ -60,46 +62,36 @@ public class SocketController implements ISocketController
 	public void run() 
 	{
 		//TODO some logic for listening to a socket //(Using try with resources for auto-close of socket)
-		try (ServerSocket listeningSocket = new ServerSocket(Port)){ 
-			while (true){
+		try (ServerSocket listeningSocket = new ServerSocket(Port))
+		{ 
+			while (true)
+			{
 				waitForConnections(listeningSocket); 	
 			}		
 		} catch (IOException e1) {
 			// TODO Maybe notify MainController?
 			e1.printStackTrace();
 		} 
-
-
 	}
 
-	private void waitForConnections(ServerSocket listeningSocket) {
+	private void waitForConnections(ServerSocket listeningSocket) 
+	{
 		try {
 			Socket activeSocket = listeningSocket.accept();
+			new SocketThread(activeSocket).start();
+			
+			
+			/*
+			 
 			String inLine;
 			inStream = new BufferedReader(new InputStreamReader(activeSocket.getInputStream()));
 			outStream = new DataOutputStream(activeSocket.getOutputStream());
-//			new Thread() 
-//			{
-//				public void run() 
-//				{
-//					try
-//					{
-//						inStream = new BufferedReader(new InputStreamReader(activeSocket.getInputStream()));
-//						outStream = new DataOutputStream(activeSocket.getOutputStream());
-//
-//					}
-//					catch(Exception e)
-//					{
-//						System.err.println(e);
-//					}
-//				}
-//			}.start();
 
 			//.readLine is a blocking call 
 			//TODO How do you handle simultaneous input and output on socket?
 			//TODO this only allows for one open connection - how would you handle multiple connections?
-			//ServerThread st = new ServerThread(activeSocket); // SKal m�ske laves en Thread //
-
+		
+			
 			while (true){
 				inLine = inStream.readLine();
 				System.out.println(inLine);
@@ -166,35 +158,138 @@ public class SocketController implements ISocketController
 					break;
 				//TODO implementation done!
 				}
-			}
+			}*/
 		} catch (IOException e) {
 			//TODO maybe notify mainController?
 			e.printStackTrace();
 		}
 	}
 
-	private void notifyObservers(SocketInMessage message) {
-		for (ISocketObserver socketObserver : observers) {
+	public void notifyObservers(SocketInMessage message) 
+	{
+		for (ISocketObserver socketObserver : observers) 
+		{
 			socketObserver.notify(message);
 		}
 	}
 
-	private static boolean isItANumber(String x){
+	public boolean isItANumber(String x)
+	{
 		boolean b = true;
 		String s = x;
 		int dotCount = 0;
 		for(int i = 0; i < s.length(); i++){
-			if(s.charAt(i) >= 48 && s.charAt(i) <= 57) {
+			if(s.charAt(i) >= 48 && s.charAt(i) <= 57) 
+			{
 			}
-			else if(s.charAt(i) == 46){
+			else if(s.charAt(i) == 46)
+			{
 				dotCount++;
 			}
 			else b = false;
 		}
-		if(dotCount > 1){
+		if(dotCount > 1)
+		{
 			b = false;
 		}
 		return b;
 	}
+}
+class SocketThread extends Thread 
+{
+	  Socket activeSocket;
+	  String inLine;
+	  
+	  private BufferedReader inStream;
+	  private DataOutputStream outStream;
+	  
+	  SocketController CS = new SocketController();
+	  
+	  public SocketThread(Socket activeSocket) 
+	  {
+	    this.activeSocket = activeSocket;
+	  }
+
+	  public void run() 
+	  {
+	    try 
+	    {
+	    	inStream = new BufferedReader(new InputStreamReader(activeSocket.getInputStream()));
+	   	    outStream = new DataOutputStream(activeSocket.getOutputStream());
+	    	
+	    	while (true)
+	    	{
+				inLine = inStream.readLine();
+				System.out.println(inLine);
+				if (inLine==null) break;
+				switch (inLine.split(" ")[0]) 
+				{
+				case "RM20": // Display a message in the secondary display and wait for response
+					//TODO implement logic for RM command
+					if(inLine.split(" ")[1].equals("8"))
+					{
+						try 
+						{
+							CS.notifyObservers(new SocketInMessage(SocketMessageType.RM208, inLine.split("8")[1]));
+						System.out.println("Du har skrevet RM208");
+						}
+						catch (ArrayIndexOutOfBoundsException e) 
+						{
+							CS.notifyObservers(new SocketInMessage(SocketMessageType.RM208, "INDTAST NR"));
+						}
+					}
+					else if(inLine.split(" ")[1].equals("4"))
+					{
+						CS.notifyObservers(new SocketInMessage(SocketMessageType.RM204, inLine.split("8")[1]));
+						System.out.println("Du har skrevet RM204");
+					}
+					else 
+						System.out.println("Du har tastet forkert.");
+					break;
+				case "D":// Display a message in the primary display
+					//TODO Refactor to make sure that faulty messages doesn't break the system					
+					if(CS.isItANumber(inLine.split(" ")[1])){
+						CS.notifyObservers(new SocketInMessage(SocketMessageType.D, inLine.split(" ")[1])); 
+					}
+					break;
+				case "DW": //Clear primary display
+					CS.notifyObservers(new SocketInMessage(SocketMessageType.DW, inLine.split(" ")[0]));
+					//TODO implement
+					break;
+				case "P111": //Show something in secondary display
+					CS.notifyObservers(new SocketInMessage(SocketMessageType.P111, inLine.split(" ")[1]));
+					//TODO implement
+					break;
+				case "T": // Tare the weight
+					CS.notifyObservers(new SocketInMessage(SocketMessageType.T, inLine.split(" ")[1]));
+					//TODO implement
+					break;
+				case "S": // Request the current load
+					CS.notifyObservers(new SocketInMessage(SocketMessageType.S, inLine.split(" ")[1]));
+					//TODO implement
+					break;
+				case "K":
+					if (inLine.split(" ").length>1){
+						CS.notifyObservers(new SocketInMessage(SocketMessageType.K, inLine.split(" ")[1]));
+					}
+					break;
+				case "B": // Set the load
+					//TODO implement
+					break;
+				case "Q": // Quit
+					//TODO implement
+					break;
+				default: //Something went wrong?
+					//TODO implement
+					break;
+				}
+			}
+	    } 
+	    catch (IOException e) 
+	    {
+	      System.out.println(e);
+	    }
+
+	  }
 }
 
