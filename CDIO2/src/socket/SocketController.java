0fe1563fe1fa6fd0929f6bd759f45bb8111f3523
Stuart <Stuart@192.168.0.10>
2017-03-16 21:01:43 +0100

    Dværge kræfter

--- CDIO2/src/socket/SocketController.java
+++ CDIO2/src/socket/SocketController.java
@@ -8,7 +8,9 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import socket.SocketInMessage.SocketMessageType;
@@ -17,6 +19,8 @@ public class SocketController implements ISocketController
 {
 	Set<ISocketObserver> observers = new HashSet<ISocketObserver>();
 	//TODO Maybe add some way to keep track of multiple connections?
+	Map<String, SocketThread> connectedClients = new HashMap<String, SocketThread>();
+	
 	private BufferedReader inStream;
 	private DataOutputStream outStream;
 
@@ -75,88 +79,10 @@ public class SocketController implements ISocketController
 
 	private void waitForConnections(ServerSocket listeningSocket) 
 	{
-		try {
+		try 
+		{
 			Socket activeSocket = listeningSocket.accept();
 			new SocketThread(activeSocket, this).start();
-			
-			//tis
-			/*
-			String inLine;
-			inStream = new BufferedReader(new InputStreamReader(activeSocket.getInputStream()));
-			outStream = new DataOutputStream(activeSocket.getOutputStream());
-
-			//.readLine is a blocking call 
-			//TODO How do you handle simultaneous input and output on socket?
-			//TODO this only allows for one open connection - how would you handle multiple connections?
-		
-			
-			while (true){
-				inLine = inStream.readLine();
-				System.out.println(inLine);
-				if (inLine==null) break;
-				switch (inLine.split(" ")[0]) {
-				case "RM20": // Display a message in the secondary display and wait for response
-					//TODO implement logic for RM command
-					if(inLine.split(" ")[1].equals("8"))
-					{
-						try {
-							notifyObservers(new SocketInMessage(SocketMessageType.RM208, inLine.split("8")[1]));
-							System.out.println("Du har skrevet RM208");
-						}
-						catch (ArrayIndexOutOfBoundsException e) {
-							notifyObservers(new SocketInMessage(SocketMessageType.RM208, "INDTAST NR"));
-						}
-					}
-					else if(inLine.split(" ")[1].equals("4"))
-					{
-						notifyObservers(new SocketInMessage(SocketMessageType.RM204, inLine.split("8")[1]));
-						System.out.println("Du har skrevet RM204");
-					}
-					else 
-						System.out.println("Du har tastet forkert.");
-					break;
-				case "D":// Display a message in the primary display
-					//TODO Refactor to make sure that faulty messages doesn't break the system					
-					notifyObservers(new SocketInMessage(SocketMessageType.D, inLine.split(" ")[1])); 
-					break;
-				case "DW": //Clear primary display
-					notifyObservers(new SocketInMessage(SocketMessageType.DW, "DW"));
-					//TODO implementation done!
-					break;
-				case "P111": //Show something in secondary display
-					notifyObservers(new SocketInMessage(SocketMessageType.P111, inLine.split(" ")[1]));
-					//TODO implementation done!
-					break;
-				case "T": // Tare the weight
-					notifyObservers(new SocketInMessage(SocketMessageType.T, "T"));
-					//TODO implementation done!
-					break;
-				case "S": // Request the current load
-					notifyObservers(new SocketInMessage(SocketMessageType.S, "S"));
-					//TODO implementation done!
-					break;
-				case "K":
-					if (inLine.split(" ").length>1){
-						notifyObservers(new SocketInMessage(SocketMessageType.K, inLine.split(" ")[1]));
-					}
-					break;
-				case "B": // Set the load
-					if(isItANumber(inLine.split(" ")[1])){
-						notifyObservers(new SocketInMessage(SocketMessageType.D, inLine.split(" ")[1]));
-					//TODO implementation done!
-					}
-					//TODO implement
-					break;
-				case "Q": // Quit
-					notifyObservers(new SocketInMessage(SocketMessageType.DW, "Q"));
-					//TODO implementation done!
-					break;
-				default: //Something went wrong?
-					notifyObservers(new SocketInMessage(SocketMessageType.wrongCommand, "wrongCommand"));
-					break;
-				//TODO implementation done!
-				}
-			}*/
 		} 
 		catch (IOException e) 
 		{
@@ -214,12 +140,14 @@ class SocketThread extends Thread // Denne klasse kan bare slåes fra //
 	  public void run() 
 	  {
 		  String inLine;
-		 
+		  
+		  SC.connectedClients.put(activeSocket.getInetAddress().toString(), this);
+		  
 		  try 
 		  {
 	    	inStream = new BufferedReader(new InputStreamReader(activeSocket.getInputStream()));
 	   	    outStream = new DataOutputStream(activeSocket.getOutputStream());
-	    	
+	   	   
 	   	    while (true)
 	    	{
 	    		inLine = inStream.readLine();
